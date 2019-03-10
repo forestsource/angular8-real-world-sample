36b171f92b57c44aaf0405e20b80ca164318633f
forestsource <forestsources@gmail.com>
2019-03-11 01:44:12 +0900

    movie card
    multiple sample

--- src/app/services/mockes/mock-movies.ts
+++ src/app/services/mockes/mock-movies.ts
@@ -1,7 +1,22 @@
 import { Movie } from '../movie';
 
 export const MOVIES: Movie[] = [
-    { id: 1, title: 'Hero', thumbnail: '/assets/images/mock.jpg' },
-    { id: 2, title: 'get it', thumbnail: '/assets/images/mock.jpg' },
-    { id: 3, title: 'out of controle', thumbnail: '/assets/images/mock.jpg' }
+  { id: 1, title: 'Hero', thumbnail: 'assets/images/mock.jpg' },
+  { id: 2, title: 'get it', thumbnail: 'assets/images/mock.jpg' },
+  { id: 3, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 4, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 5, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 6, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 7, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 8, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 9, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 10, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 11, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 12, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 13, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 14, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 15, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 16, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 17, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' },
+  { id: 18, title: 'out of controle', thumbnail: 'assets/images/mock.jpg' }
 ];
